4281fd131f0652c9dc5027a94a1275af1ad8e2c3
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-05-23 14:12:01 -0400

    text

---
+++ src/components/people.js
@@ -0,0 +1,33 @@
+const People = () => {
+    return(
+        <div>
+            <div>
+                <h4>Our People</h4>
+                <p>We bring the right people together to solve environmental challenges 
+                    from many different angles. Meet some of the experts who represent 
+                    key parts of our work.</p>
+            </div>
+            <div>
+                <div>
+                    <p>quote image</p>
+                    <p>partner persceptive</p>
+                    <p>EDF does constructive, thoughtful, hard work, and that gains respect — and results.</p>
+                    <p>George P. Shultz</p>
+                    <p>Former U.S. Secretary of State</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>Our Board</li>
+                        <li>Exdcutive Team</li>
+                        <li>Work With us</li>
+                    </ul>
+                </div>
+               
+            </div>
+            
+        </div>
+        
+    )
+}
+
+export default People
