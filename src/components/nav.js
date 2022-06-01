d19b228af9001aaeac2df84a4a0929d2a95b64d4
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-06-01 15:40:35 -0400

    hamburger

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,31 @@
+import {Link} from "react-router-dom"
+import { useState } from "react"
+
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 const Nav = () => {
+        const [navbarOpen, setNavbarOpen] = useState(false)
+        const handleToggle = () => {
+            setNavbarOpen(!navbarOpen)
+            }
     return(
-        <nav>
-            <h4 className='work'>Our Work</h4>
-            <h4 className = 'involve'> Get Involved</h4>
-            <h4 className = 'About'>About</h4>
-            <h4 className = 'Donate'> Donate</h4>
-            <h3>searchbar</h3>
-        </nav>
+        <div className = 'nav'>
+            <nav className = 'navBar'>
+                <button onClick={handleToggle}>
+                    {navbarOpen ? (<MdClose className = 'bar1' size={25}/>):
+                    (<FiMenu className = 'bar2' size={25} />)}
+                </button>
+                <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>
+                    <h4 className='work'>Our Work</h4>
+                    <h4 className = 'involve'> Get Involved</h4>
+                    <h4 className = 'About'>About</h4>
+                    <h4 className = 'Donate'> Donate</h4>
+                    <h3>searchbar</h3>
+                </ul>
+                
+            </nav>
+           
+        </div>
         
     )
 }
--- src/components/people.js
+++ src/components/people.js
@@ -1,4 +1,8 @@
+import {Link} from "react-router-dom"
+import { useState } from "react"
+
 const People = () => {
+          
     return(
         <div>
             <div>
